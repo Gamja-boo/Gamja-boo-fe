643b1a255d76431ec4341112ab5cb51876f9fc5f
junyi04 <amber542825@gmail.com>
2025-05-05 23:23:02 +0900

    feat: 커스터마이징 바 구현

--- app/screens/character_screen/CustomizingScreen.tsx
+++ app/screens/character_screen/CustomizingScreen.tsx
@@ -1,10 +1,54 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import CustomizingBar from "@/components/CustomizingBar";
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
 export default function CustomizingScreen() {
   return (
-    <View>
-      <Text>커스터마이징 화면</Text>
+    <View style={styles.container}>
+      <View style={styles.customizingBarContainer}>
+        <CustomizingBar />
+      </View>
+
+      {/* 화면 가운데 십자선 가이드 라인: x축 */}
+      <View
+        style={{
+          position: "absolute",
+          top: screenHeight / 2,
+          left: 0,
+          width: screenWidth,
+          height: 1,
+          backgroundColor: "red",
+          zIndex: 3,
+        }}
+      />
+      {/* 화면 가운데 십자선 가이드 라인: y축 */}
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: screenWidth / 2,
+          width: 1,
+          height: screenHeight,
+          backgroundColor: "red",
+          zIndex: 3,
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FCFFF6",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  customizingBarContainer: {
+    position: "absolute",
+    bottom: screenHeight * 0.46,
+    backgroundColor: "transparent",
+  },
+}) 
---
+++ components/CustomizingBar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
+import Skin from "@/app_assets/customizing_bar/skin.svg"
+import Cloth from "@/app_assets/customizing_bar/cloth.svg"
+import Accessory from "@/app_assets/customizing_bar/accessory.svg"
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+
+export default function CustomizingBar() {
+  return (
+    <View style={styles.customizingBar}>
+      <TouchableOpacity>
+        <Skin height={screenWidth * 0.083} width={screenWidth * 0.083}/>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Cloth height={screenWidth * 0.08} width={screenWidth * 0.08}/>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Accessory height={screenWidth * 0.08} width={screenWidth * 0.08}/>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  customizingBar: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    height: screenHeight * 0.08,
+    width: screenWidth * 0.65,
+    backgroundColor: "white",
+    borderTopLeftRadius: screenHeight * 0.05,
+    borderTopRightRadius: screenHeight * 0.05,
+    borderBottomRightRadius: screenHeight * 0.05,
+    borderBottomLeftRadius: screenHeight * 0.05,
+    // shadow at ios
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: -5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    // shadow at Android
+    elevation: 5,
+  }
+})
