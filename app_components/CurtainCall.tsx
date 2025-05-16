240073e1d54b74a20daa39cd25609e53cb8601f7
junyi04 <amber542825@gmail.com>
2025-05-16 15:06:45 +0900

    feat: 커튼콜 컴포넌트 만드는 중

---
+++ app_components/CurtainCall.tsx
@@ -0,0 +1,40 @@
+import { View, Text, StatusBar, StyleSheet, Dimensions } from "react-native";
+import GestureRecognizer from 'react-native-swipe-gestures';
+
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+
+interface Props {
+  onSwipeDown: () => void;
+};
+
+export function CurtainCall({ onSwipeDown }: Readonly<Props>) {
+
+  const onSwipe = (gestureName: string) => {
+    if (gestureName === 'SWIPE_DOWN') {
+      onSwipeDown();
+    }
+  };
+  
+  return (
+    <GestureRecognizer
+      onSwipe={onSwipe}
+      style={styles.container}
+      >
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+    </GestureRecognizer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: screenWidth,
+    height: screenHeight,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+    text: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+})
