01cea778fc1b641914f1370f5c5ac88659fc4912
이병길 <qudrlf72@naver.com>
2025-07-17 15:43:16 +0900

    chore: 빌드 환경 구성

--- app.config.js
+++ app.config.js
@@ -1,9 +1,30 @@
 import 'dotenv/config';
 
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    env: process.env.ENV,
-    apiBaseUrl: process.env.API_BASE_URL,
+export default {
+  expo: {
+    name: 'GamJa-boo',
+    slug: 'gamja-boo',
+    version: '1.0.0',
+    owner: 'byunggillee',
+    icon: './assets/gamjaMainCharacter.png',
+    splash: {
+      image: './assets/gamjaMainCharacter.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    assetBundlePatterns: ['**/*'],
+    android: {
+      package: 'com.pnu_apptive.gamjaboo',
+    },
+    ios: {
+      bundleIdentifier: 'com.pnu_apptive.gamjaboo',
+    },
+    extra: {
+      env: process.env.ENV,
+      apiBaseUrl: process.env.API_BASE_URL,
+      eas: {
+        projectId: '8c9e3900-f24f-4f52-ae3a-e8f8e8c4ea90',
+      },
+    },
   },
-});
+};
